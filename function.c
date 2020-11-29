15f51f1097789efa005fb4b644050140e24d399e
ericfreenda8793 <74149309+ericfreenda8793@users.noreply.github.com>
2020-11-29 16:10:25 +0800

    Add files via upload

---
+++ function.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int** s(int* a,int len){
+
+int t;
+   int temp=0;
+    for(int i=0;i<len-1;i++){
+        for(int j=0;j<len-1-i;j++){
+            if(*(a+j)>*(a+j+1)){
+                temp=*(a+j+1);
+                *(a+j+1)=*(a+j);
+                *(a+j)=temp;
+            }
+        }
+    }
+
+
+int **ans;
+int n= (int)sqrt(len);
+ans=(int **)malloc(n*sizeof(*ans));
+ans[0]=(int *)malloc(n*n*sizeof(int));
+
+for(int i=0;i<n;i++){
+        ans[i]=&ans[0][i*n];
+}
+int count=0;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        ans[i][j]=*(a+count);
+        count++;
+    }
+}
+
+return ans;
+
+}
